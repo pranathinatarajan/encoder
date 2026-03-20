6fa82391c9180332eb3b098b6cc798570871d8c7
pranathinatarajan <pranathinatarajan236@gmail.com>
2026-03-20 18:44:00 +0600

    Main decoding loop added

--- encoder.py
+++ encoder.py
@@ -17,3 +17,15 @@ phase_shift = np.pi / 2
 sin_waveB = np.sin(2*np.pi*frequency*t + phase_shift)
 square_waveB = np.sign(sin_waveB)
 converted_waveB = (square_waveB + 1)/2
+
+#main loop
+current_position = 0
+positions = []
+for i in range (1, len(converted_waveA)):
+    if converted_waveA[i-1] == 0 and converted_waveA[i] == 1:
+        if converted_waveB[i] == 1:
+            current_position = current_position + 1
+        else:
+            current_position = current_position - 1
+    positions.append(current_position)
+
